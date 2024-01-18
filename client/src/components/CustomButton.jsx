4b1b15856e313aeaa34dfed3ed46f223bcf582dd
KakwiriStephen <ngottakakwiri@gmail.com>
2024-01-18 16:03:23 +0300

    Navbar

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CustomButton = ({ btnType, title, handleCick, styles }) => {
+  return (
+    <button
+      type={btnType}
+      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      onClick={handleCick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
